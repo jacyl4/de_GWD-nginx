dd9f2da725b51598ed2f8c83626850792242180d
JacyL4 <jacyl4@gmail.com>
2020-12-02 01:16:08 +0800

    auto

--- nginx_amd64.sh
+++ nginx_amd64.sh
@@ -48,7 +48,7 @@ tar -zxvf pcre-8.44.tar.gz
 
 wget https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
 tar zxvf nginx-$NGINX_VERSION.tar.gz
-sed -i 's/CFLAGS="$CFLAGS -g"/#CFLAGS="$CFLAGS -g"/' /tmp/src/nginx-$NGINX_VERSION/auto/cc/gcc
+sed -i 's/CFLAGS="$CFLAGS -g"/#CFLAGS="$CFLAGS -g"/' nginx-$NGINX_VERSION/auto/cc/gcc
 mv -f nginx-$NGINX_VERSION nginx
 cd nginx
 curl https://raw.githubusercontent.com/kn007/patch/master/nginx_with_quic.patch | patch -p1

a95a8b3431c6e22656203d08f45b0d0c1968b9f8
JacyL4 <jacyl4@gmail.com>
2021-01-14 17:29:15 +0800

    Update doh.sh

--- doh.sh
+++ doh.sh
@@ -2,8 +2,8 @@
 GO_VERSION="1.15.6"
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install --no-install-recommends --no-install-suggests -y wget curl ca-certificates rsync unzip git build-essential
+apt-get update
+apt-get install --no-install-recommends --no-install-suggests -y wget curl ca-certificates rsync unzip git build-essential
 
 architecture=""
 case $(uname -m) in
@@ -14,19 +14,19 @@ esac
 if [[ $architecture = "amd64" ]]; then
     wget -N https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
 	tar -xvf go*linux-amd64.tar.gz
-	sudo rm -rf go*linux-amd64.tar.gz
+	rm -rf go*linux-amd64.tar.gz
 elif [[ $architecture = "arm64" ]]; then
     wget -N https://dl.google.com/go/go$GO_VERSION.linux-arm64.tar.gz
 	tar -xvf go*linux-arm64.tar.gz
-	sudo rm -rf go*linux-arm64.tar.gz
+	rm -rf go*linux-arm64.tar.gz
 fi
 
-sudo mkdir -p $HOME/gopath
-sudo mv -f go /usr/local
+mkdir -p $HOME/gopath
+mv -f go /usr/local
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/gopath
 export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
 
 git clone https://github.com/m13253/dns-over-https
 cd dns-over-https*
-sudo make && sudo make install
+make && make install

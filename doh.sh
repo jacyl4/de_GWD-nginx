0173d19fe9a1c117356f9f73cd9d75c620369343
JacyL4 <jacyl4@gmail.com>
2021-03-03 20:45:08 +0800

    auto

--- doh.sh
+++ doh.sh
@@ -5,17 +5,11 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install --no-install-recommends --no-install-suggests -y wget curl ca-certificates rsync unzip git build-essential
 
-architecture=""
-case $(uname -m) in
-    x86_64)  architecture="amd64" ;;
-    aarch64)  architecture="arm64" ;;
-esac
-
-if [[ $architecture = "amd64" ]]; then
+if [[ $(dpkg --print-architecture) = "amd64" ]]; then
     wget -N https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
 	tar -xvf go*linux-amd64.tar.gz
 	rm -rf go*linux-amd64.tar.gz
-elif [[ $architecture = "arm64" ]]; then
+elif [[ $(dpkg --print-architecture) = "arm64" ]]; then
     wget -N https://dl.google.com/go/go$GO_VERSION.linux-arm64.tar.gz
 	tar -xvf go*linux-arm64.tar.gz
 	rm -rf go*linux-arm64.tar.gz
